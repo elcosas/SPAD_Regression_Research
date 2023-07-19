b1fb8fa2219eb3241c63524e5a1f6ddcc752ae3d
elcosas <121999809+elcosas@users.noreply.github.com>
2023-07-19 09:26:09 -0700

    Re-organized experiments directory into testing directory

---
+++ Tests/Single-Hist-Test/single-hist-test.py
@@ -0,0 +1,39 @@
+import importlib
+import numpy as np
+#from numpy.polynomial import Polynomial
+import matplotlib.pyplot as plt
+from sys import argv, exit
+
+def main():
+    """if len(argv) != 2:
+        print('Syntax Error: Please provide 1 valid package name')
+        exit()
+     """   
+    # Load dataset
+    bucket_transients = np.load('../Prop_Notebooks/edh_boundaries.npy')
+
+    # Load module w/ regression method
+    """
+    try:
+        importlib.import_module(argv[1])
+    except:
+        print('Load Error: Invalid package name')
+    """
+
+    # set constants
+    BINS = 16
+
+    #####################################################
+
+    bt_set = bucket_transients[1, :]
+    plt.hist(bt_set, bins=BINS)
+
+    bt_hist = np.histogram(bt_set, bins=BINS)
+
+    test_poly_eq = np.poly1d(np.polyfit(bt_set, bt_hist[0], 15))
+    plt.plot(bt_set, test_poly_eq(bt_set), 'r')
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
